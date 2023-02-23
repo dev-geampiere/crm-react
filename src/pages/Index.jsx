358b8bec3ef26b0247a037fde7e811335634cc26
dev-geampiere <jeanpiere.virgilio@gmail.com>
2023-02-23 14:05:24 -0500

    Creando un loader

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+export function loader() {
+  
+  return 'desde loader'
+}
 
 const Index = () => {
   return (
-    <div>Cliente</div>
+    <>
+      <h1 className='text-4xl font-black text-blue-900'>CLientes</h1>
+      <p className='mt-3'>Administra a tus clientes</p>
+    </>
   )
 }
 
