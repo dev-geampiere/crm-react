80c86dadf4cdc1d086a6b32e0280452934369cb9
dev-geampiere <jeanpiere.virgilio@gmail.com>
2023-02-24 13:34:16 -0500

    Mostrando el resto de la informacion

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -3,8 +3,27 @@ function Cliente({cliente}) {
 
   return (
     <tr>
-      <td className='p-6'>
-        {nombre}
+      <td className='p-6 space-y-2'>
+        <p className="text-2xl text-gray-800">{nombre}</p>
+        <p>{empresa}</p>
+      </td>
+      <td className="p-6">
+        <p className="text-gray-600"> <span className="text-gray-800 uppercase font-bold">Email: </span> {email} </p>
+        <p className="text-gray-600"> <span className="text-gray-800 uppercase font-bold">Tel: </span> {telefono} </p>
+      </td>
+      <td className="p-6 flex gap-3">
+        <button
+          type="button"
+          className="text-blue-700 hover:text-blue-800 uppercase font-bold text-xs"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="text-red-700 hover:text-red-800 uppercase font-bold text-xs"
+        >
+          Eliminar
+        </button>
       </td>
     </tr>
   )
